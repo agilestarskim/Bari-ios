5e9400d663d031456d13fe144538ef06e2c42093
agilestarskim <agilestarskim@naver.com>
2022-12-20 13:19:17 +0900

    Initial Commit

---
+++ Bari/BariApp.swift
@@ -0,0 +1,17 @@
+//
+//  BariApp.swift
+//  Bari
+//
+//  Created by 김민성 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bari/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bari
+//
+//  Created by 김민성 on 2022/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
